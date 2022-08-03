47370e1b0ead5f334d0c95659f51713a8b2dfdf4
Rohit-231 <pal8035@gmail.com>
2022-08-03 15:44:24 +0530

    second api

--- src/main/java/com/springboot/microservices/controller/HomeController.java
+++ src/main/java/com/springboot/microservices/controller/HomeController.java
@@ -6,9 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 	
+	@GetMapping("/")
+	public String GetHomepage() {
+		return "Hello World: Homepage";
+	}
+	
 	@GetMapping("/hello")
 	public String GetHello() {
 		return "Hello World: inside My App";
+	}
+	
+	@GetMapping("/cloud")
+	public String GetCloud() {
+		return "Hello Cloud: inside My App";
 	}
 
 }
