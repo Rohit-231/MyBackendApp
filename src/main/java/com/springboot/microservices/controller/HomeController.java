1970830dfd75a9704d6cfc00a7fcea80187de131
Rohit-231 <pal8035@gmail.com>
2022-08-03 15:19:21 +0530

    first insertion jira completed

---
+++ src/main/java/com/springboot/microservices/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.springboot.microservices.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/hello")
+	public String GetHello() {
+		return "Hello World: inside My App";
+	}
+
+}
